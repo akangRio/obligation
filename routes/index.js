98d07bb2854b37d78630716cc95c6086b95240e4
akangRio <fachryo97@gmail.com>
2024-04-18 23:18:50 +0700

    add errorHandler

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userController = require("../controllers/userController");
 const instituteController = require("../controllers/insituteController");
 const authenticating = require("../middleware/authenticator");
+const errorHandler = require("../middleware/errorHandler");
 const router = express.Router();
 
 router.post("/login", userController.login);
@@ -11,5 +12,7 @@ router.use(authenticating);
 router.post("/", userController.createUser);
 router.post("/institute", instituteController.createInstitute);
 router.put("/institute/adduser", instituteController.addUser);
+
+router.use(errorHandler);
 
 module.exports = router;
